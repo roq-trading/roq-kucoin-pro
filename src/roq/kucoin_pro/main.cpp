03eef9bf9f3314e19451fe2aafc2dd14b059ebea
thraneh <thraneh@gmail.com>
2026-05-13 14:24:35 +0000

    chore

--- src/roq/kucoin_pro/main.cpp
+++ src/roq/kucoin_pro/main.cpp
@@ -11,7 +11,7 @@ using namespace std::literals;
 
 namespace {
 auto const INFO = roq::Service::Info{
-    .description = "Roq KuCoin Futures Gateway"sv,
+    .description = "Roq's KuCoin PRO gateway"sv,
     .package_name = ROQ_PACKAGE_NAME,
     .host = ROQ_HOST,
     .build_version = ROQ_BUILD_VERSION,
